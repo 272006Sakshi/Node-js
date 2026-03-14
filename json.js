84de38e25bc0d035a5e454cb8808ac779b97d7cd
272006Sakshi <sakshi_2312res563@iitp.ac.in>
2026-03-14 12:51:57 +0530

    update file

--- json.js
+++ json.js
@@ -2,7 +2,8 @@ const jsonString = '{"name": "Sakshi","age": 19, "city": "Muzaffarpur"}';
 const jsonObject = JSON.parse(jsonString); // convert json string to object
 console.log(jsonObject.name); // output- sakshi
 
-const objectToconvert = {name: "Sakshi", age: 25};
+const objectToconvert = {name: "Sakshi", age: 19};
 const jsonStringified = JSON.stringify(objectToconvert); // convert object to json string
-console.log(jsonStringified); // output- {"name":"Sakshi","age":25}
+console.log(jsonStringified); // output- {"name":"Sakshi","age":19}
 console.log(typeof jsonStringified);
+ 
